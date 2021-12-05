6800427fde77777a2f21d9debbd57c46be5a76bd
WILL <WIM.TRD@gmail.com>
2021-12-05 18:58:28 +0900

    Commit-5-part1: Exercise 1.3 course information step3.

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -6,43 +6,37 @@ const Header = (props) => {
 }
 
 const Part = (props) => {
-  return <p>{props.part}: {props.exercise}</p>
+  return <p>{props.item.name}: {props.item.exercises}</p>
 }
 
 const Content = (props) => {
     const content = props.content.map((item) => {
-        return <Part part = {item.part} exercise={item.exercise} />})
+        return <Part item = {item} />})
     return ( <div> {content} </div>
       )
     }
 
   const Total = (props) => {
-      const total = props.content.reduce((a, v) => a = a + v.exercise, 0)
+      const total = props.content.reduce((a, v) => a = a + v.exercises, 0)
       return ( <p> Number of exercises {total} </p>
       )
     }
 
   const App = () => {
       const course = 'Half Stack application development'
-      const part1 = 'Fundamentals of React'
-      const exercises1 = 10
-      const part2 = 'Using props to pass data'
-      const exercises2 = 7
-      const part3 = 'State of a component'
-      const exercises3 = 14
-      const content = [{
-          part: part1,
-          exercise: exercises1
-        },
-        {
-          part: part2,
-          exercise: exercises2
-        },
-        {
-          part: part3,
-          exercise: exercises3
-        }
-      ];
+      const part1 = {
+        name: 'Fundamentals of React',
+        exercises: 10
+      }
+      const part2 = {
+        name: 'Using props to pass data',
+        exercises: 7
+      }
+      const part3 = {
+        name: 'State of a component',
+        exercises: 14
+      }
+      const content = [part1,part2,part3];
 
       return (
         <div>
