a5273cbf2db2e83441a97d19cbf398e650238914
WILL <WIM.TRD@gmail.com>
2021-12-05 18:39:32 +0900

    Commit-3-part1: Applying "beautify" on exercise 1.1-1.2 code.

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,50 +1,82 @@
 import React from 'react'
 
 const Header = (props) => {
-  return (
-    <h1>{props.course}</h1>
+  return ( <
+    h1 > {
+      props.course
+    } < /h1>
   )
 }
 
 const Part = (props) => {
-  return <p>{props.item.part}: {props.item.exercise}</p>
+  return <p > {
+    props.item.part
+  }: {
+    props.item.exercise
+  } < /p>
 }
 
 const Content = (props) => {
-  const content = props.content.map((item) =>
-      { return <Part item={item} />}
-  )
-  return(
-    <div>{content}</div>
-  )
-}
+    const content = props.content.map((item) => {
+        return <Part item = {
+          item
+        }
+        />}
+      ) return ( <
+        div > {
+          content
+        } < /div>
+      )
+    }
 
-const Total = (props) => {
-  const total = props.content.reduce((a,v) =>  a = a + v.exercise , 0 )
-  return(
-    <p>Number of exercises {total}</p>
-  )
-}
+    const Total = (props) => {
+      const total = props.content.reduce((a, v) => a = a + v.exercise, 0)
+      return ( <
+        p > Number of exercises {
+          total
+        } < /p>
+      )
+    }
 
-const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-  const content = [{part:part1, exercise:exercises1},
-                   {part:part2, exercise:exercises2},
-                   {part:part3, exercise:exercises3}];
+    const App = () => {
+      const course = 'Half Stack application development'
+      const part1 = 'Fundamentals of React'
+      const exercises1 = 10
+      const part2 = 'Using props to pass data'
+      const exercises2 = 7
+      const part3 = 'State of a component'
+      const exercises3 = 14
+      const content = [{
+          part: part1,
+          exercise: exercises1
+        },
+        {
+          part: part2,
+          exercise: exercises2
+        },
+        {
+          part: part3,
+          exercise: exercises3
+        }
+      ];
 
-  return (
-    <div>
-      <Header course={course} />
-      <Content content={content} />
-      <Total content={content} />
-    </div>
-  )
-}
+      return ( <
+        div >
+        <
+        Header course = {
+          course
+        }
+        /> <
+        Content content = {
+          content
+        }
+        /> <
+        Total content = {
+          content
+        }
+        /> <
+        /div>
+      )
+    }
 
-export default App
+    export default App
