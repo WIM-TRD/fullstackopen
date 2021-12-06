39f0506382076cb9c4e8c8f2fd326bf46f6bfbff
WILL <WIM.TRD@gmail.com>
2021-12-06 18:35:18 +0900

    Commit-9-part1: Exercise 1.7

--- part1/unicafe/README.md
+++ part1/unicafe/README.md
@@ -2,3 +2,4 @@
 
 The app contains exercises 1.6.-1.14 for part1:
 - 1.6: unicafe step1
+- 1.7: unicafe step2
--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -16,8 +16,24 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [value, setValue] = useState(10)
   const incrementValue = (value, setFunction) => {
     setFunction(value+1)
+  }
+  const calculateAverageScore = (good, neutral, bad) => {
+     let ret_val = ((good-bad)/(good+neutral+bad))
+     if (isNaN(ret_val)) {
+       ret_val = 0
+     }
+     return ret_val
+  }
+  const calculatePositiveFeedback = (good, neutral, bad) => {
+     let ret_val = (good)/(neutral + bad + good)
+
+     if (isNaN(ret_val)) {
+       ret_val = 0
+     }
+     return (ret_val*100) + "%"
   }
   const _good = "Good"
   const _neutral = "Neutral"
@@ -26,7 +42,6 @@ const App = () => {
   return (
     <div>
       <Header title = {header_welcome} />
-
       <Button handleClick={()=>incrementValue(good, setGood)} text={_good} />
       <Button handleClick={()=>incrementValue(neutral, setNeutral)} text={_neutral} />
       <Button handleClick={()=>incrementValue(bad, setBad)} text={_bad} />
@@ -35,7 +50,11 @@ const App = () => {
       <Statistics text={_good} value={good} />
       <Statistics text={_neutral} value={neutral} />
       <Statistics text={_bad} value={bad} />
-      
+      <Statistics text={"All"} value={good+neutral+bad} />
+      <Statistics text={"Average"} value={calculateAverageScore(good,neutral,bad)} />
+      <Statistics text={"Positive"} value={calculatePositiveFeedback(good,neutral,bad)} />
+
+
     </div>
   )
 }
